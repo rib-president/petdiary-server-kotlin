776faf26bb7f800ec5742fceafc90f92d7a67f6c
rib-president <fnslfnslfnslfnslfnsl@gmail.com>
2025-06-23 16:43:06 +0900

    refresh token 갱신 및 코드 리팩토링

--- client-core/src/main/kotlin/com/petdiary/client/core/filter/InitializeFilter.kt
+++ client-core/src/main/kotlin/com/petdiary/client/core/filter/InitializeFilter.kt
@@ -1,6 +1,7 @@
 package com.petdiary.client.core.filter
 
 import com.petdiary.client.core.provider.JwtProvider
+import com.petdiary.client.core.redis.RefreshTokenRepository
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -10,7 +11,9 @@ import org.springframework.security.core.AuthenticationException
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
 
-class InitializeFilter (private val jwtProvider: JwtProvider): OncePerRequestFilter() {
+class InitializeFilter (private val jwtProvider: JwtProvider,
+    private val refreshTokenRepository: RefreshTokenRepository
+    ): OncePerRequestFilter() {
 
     override fun doFilterInternal(
         request: HttpServletRequest,
@@ -18,15 +21,23 @@ class InitializeFilter (private val jwtProvider: JwtProvider): OncePerRequestFil
         filterChain: FilterChain
     ) {
 
-        val authorization: String? = request.getHeader("Authorization");
-
-        if(authorization != null && authorization.startsWith("Bearer ")) {
-            val token = authorization.substring("Bearer ".length)
-            if (!jwtProvider.validateToken(token)) {
-                throw BadCredentialsException("expired token")
+        val authorization = request.getHeader("Authorization")
+        val token = authorization
+            ?.takeIf { it.startsWith("Bearer ") }
+            ?.substring("Bearer ".length)
+            ?.let { accessToken ->
+                if(jwtProvider.validateToken(accessToken)) {
+                    accessToken
+                } else {
+                    val userId = jwtProvider.getClaims(accessToken).subject
+                    val refreshToken = refreshTokenRepository.findByUserId(userId)?.token
+                    refreshToken?.let { jwtProvider.getAccessToken(userId) }
+                        ?: throw BadCredentialsException("expired token")
+                }
             }
 
-            val authentication: Authentication = jwtProvider.getAuthentication(token)
+        token?.let {
+            val authentication: Authentication = jwtProvider.getAuthentication(it)
             SecurityContextHolder.getContext().authentication = authentication
         }
 
--- client-core/src/main/kotlin/com/petdiary/client/core/redis/RefreshTokenRepository.kt
+++ client-core/src/main/kotlin/com/petdiary/client/core/redis/RefreshTokenRepository.kt
@@ -2,4 +2,6 @@ package com.petdiary.client.core.redis
 
 import org.springframework.data.repository.CrudRepository
 
-interface RefreshTokenRepository : CrudRepository<RefreshToken, String>
+interface RefreshTokenRepository : CrudRepository<RefreshToken, String> {
+    fun findByUserId(userId: String): RefreshToken?
+}
