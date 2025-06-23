02292a10d3c2d29948594abd7f20743d58673c65
rib-president <fnslfnslfnslfnslfnsl@gmail.com>
2025-06-23 14:28:45 +0900

    simple auth

--- client-core/src/main/kotlin/com/petdiary/client/core/filter/InitializeFilter.kt
+++ client-core/src/main/kotlin/com/petdiary/client/core/filter/InitializeFilter.kt
@@ -1,16 +1,36 @@
 package com.petdiary.client.core.filter
 
+import com.petdiary.client.core.provider.JwtProvider
 import jakarta.servlet.FilterChain
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
+import org.springframework.security.authentication.BadCredentialsException
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.AuthenticationException
+import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.OncePerRequestFilter
 
-class InitializeFilter : OncePerRequestFilter() {
+class InitializeFilter (private val jwtProvider: JwtProvider): OncePerRequestFilter() {
+
     override fun doFilterInternal(
         request: HttpServletRequest,
         response: HttpServletResponse,
         filterChain: FilterChain
     ) {
-        TODO("Not yet implemented")
+
+        val authorization: String? = request.getHeader("Authorization");
+
+        if(authorization != null && authorization.startsWith("Bearer ")) {
+            val token = authorization.substring("Bearer ".length)
+            if (!jwtProvider.validateToken(token)) {
+                throw BadCredentialsException("expired token")
+            }
+
+            val authentication: Authentication = jwtProvider.getAuthentication(token)
+            SecurityContextHolder.getContext().authentication = authentication
+        }
+
+        filterChain.doFilter(request, response)
+
     }
 }
