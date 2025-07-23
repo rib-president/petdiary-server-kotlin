079fdaeeef7cfb0d627fb65b9014dbda50c12b20
rib-president <fnslfnslfnslfnslfnsl@gmail.com>
2025-07-23 15:26:24 +0900

    GET /user/families API

--- app-platform-api/src/main/kotlin/com/petdiary/repository/impl/FamilyQueryRepositoryImpl.kt
+++ app-platform-api/src/main/kotlin/com/petdiary/repository/impl/FamilyQueryRepositoryImpl.kt
@@ -1,14 +1,19 @@
 package com.petdiary.repository.impl
 
+import com.petdiary.domain.entity.Permission
 import com.petdiary.domain.entity.QFamily
 import com.petdiary.domain.entity.QFamilyGroup
 import com.petdiary.domain.entity.QFamilyGroupUser
+import com.petdiary.domain.entity.QFamilyGroupUserPermission
+import com.petdiary.domain.entity.QPermission
 import com.petdiary.domain.entity.QPet
+import com.petdiary.domain.entity.QUser
 import com.petdiary.domain.entity.User
 import com.petdiary.dto.FamilyListResponseDto
 import com.petdiary.repository.FamilyQueryRepository
 import com.querydsl.core.types.Projections
 import com.querydsl.core.types.dsl.Expressions
+import com.querydsl.jpa.JPAExpressions
 import com.querydsl.jpa.impl.JPAQueryFactory
 
 class FamilyQueryRepositoryImpl(private val queryFactory: JPAQueryFactory): FamilyQueryRepository {
@@ -19,7 +24,16 @@ class FamilyQueryRepositoryImpl(private val queryFactory: JPAQueryFactory): Fami
                 Expressions.stringTemplate("{0}", QFamily.family.familyId),
                 QFamily.family.name,
                 QFamily.family.isDefault,
-                Expressions.nullExpression(String::class.java),
+                JPAExpressions.select(QUser("superAdmin").displayName)
+                    .from(QUser("superAdmin"))
+                    .innerJoin(QFamilyGroupUser("subGroupUser"))
+                    .on(QFamilyGroupUser("subGroupUser").user.eq(QUser("superAdmin")),
+                        QFamilyGroupUser("subGroupUser").familyGroup.eq(QFamilyGroupUser.familyGroupUser.familyGroup))
+                    .innerJoin(QFamilyGroupUserPermission.familyGroupUserPermission)
+                    .on(QFamilyGroupUserPermission.familyGroupUserPermission.familyGroupUser.eq(QFamilyGroupUser("subGroupUser")))
+                    .innerJoin(QPermission.permission)
+                    .on(QPermission.permission.eq(QFamilyGroupUserPermission.familyGroupUserPermission.permission),
+                        QPermission.permission.name.eq("SUPER-ADMIN")),
                 QPet.pet.thumbnail.location
             )
         ).from(QFamilyGroupUser.familyGroupUser)
